a5c3688f1b189c236f466617266a954590ef8264
Gaurav Dhruw <dhruwgaurav01@gmail.com>
2021-06-12 21:01:24 +0530

    package.json changed

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const port= process.env.PORT || 5000;
 
 // config for production
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static("/client/build"));
+    app.use(express.static(path.resolve(__dirname,"./client/build")));
     app.get("*",(req,res)=>{
         res.status(200).send(path.resolve(__dirname,"client","build","index.html"))
     })
